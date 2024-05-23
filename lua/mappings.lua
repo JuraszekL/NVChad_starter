eb4d1e2c95df8673edb96dcb064af4307c7d8407
L.J <109549349+JuraszekL@users.noreply.github.com>
2024-05-23 20:33:23 +0200

    add two keymaps for diagnostic

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,9 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
